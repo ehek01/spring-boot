34bfbdac06735306af00e01be984416938407add
ehek01 <whdgus0515@gmail.com>
2022-06-07 15:46:35 +0900

    타임리프로 view 랜더링

--- src/main/java/com/nhnacademy/edu/springboot/account/controller/AccountWebController.java
+++ src/main/java/com/nhnacademy/edu/springboot/account/controller/AccountWebController.java
@@ -1,5 +1,6 @@
 package com.nhnacademy.edu.springboot.account.controller;
 
+import com.nhnacademy.edu.springboot.account.entity.Account;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,10 +9,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 @Controller
 public class AccountWebController {
 
-    @GetMapping("/web/students/{id}")
+    @GetMapping("/web/account/{id}")
     public String getStudent(@PathVariable Long id, Model model) {
-//        model.addAttribute("students", );
 
-        return null;
+        model.addAttribute("account", new Account(id, "nhnBank", 2000));
+        return "account";
     }
 }
