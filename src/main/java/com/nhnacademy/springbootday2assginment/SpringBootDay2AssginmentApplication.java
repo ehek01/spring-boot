08a829f78e5096fcdac51149e04a7445e1a6d7bf
ehek01 <whdgus0515@gmail.com>
2022-06-08 16:51:44 +0900

    init

---
+++ src/main/java/com/nhnacademy/springbootday2assginment/SpringBootDay2AssginmentApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.springbootday2assginment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDay2AssginmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDay2AssginmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nhnacademy/springbootday2assginment/SpringBootDay2AssginmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.springbootday2assginment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDay2AssginmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
